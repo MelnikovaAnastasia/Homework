a6dc2424c09fb0674d899e401b2420dc243983b3
Melnikova Anastasia <luckythief13@gmail.com>
2026-07-02 10:35:37 +0400

    Поправила условие в методе makeCoffee на <=

--- src/main/java/ru/alfabank/homework11/CoffeeMachine.java
+++ src/main/java/ru/alfabank/homework11/CoffeeMachine.java
@@ -3,9 +3,9 @@ package ru.alfabank.homework11;
 public class CoffeeMachine {
 
     public void makeCoffee(int millilitersOfWater) {
-        if (millilitersOfWater < 200) {
+        if (millilitersOfWater <= 200) {
             throw new NotEnoughWaterException(
-                    "Ошибка: Для приготовления кофе необходимо не менее 200 мл воды!" +
+                    "Ошибка: Для приготовления кофе необходимо больше 200 мл воды!" +
                             "Сейчас " + millilitersOfWater + " мл воды."
             );
         }
